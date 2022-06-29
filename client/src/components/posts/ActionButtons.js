5f57a050d1913f3fb63d0303328d78e8ecafd7fc
Admin <dovancuong160700@gmail.com>
2022-06-29 13:38:09 +0700

    text edittot

--- client/src/components/posts/ActionButtons.js
+++ client/src/components/posts/ActionButtons.js
@@ -3,30 +3,50 @@ import playIcon from '../../assets/play-btn.svg'
 import editIcon from '../../assets/pencil.svg'
 import deleteIcon from '../../assets/trash.svg'
 import { PostContext } from '../../contexts/PostContext'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import './styles.css'
+import TextEditor from '../layout/TextEditor'
 
 const ActionButtons = ({ url, _id }) => {
+	const [show, setShow] = useState(false)
 	const { deletePost, findPost, setShowUpdatePostModal } = useContext(
 		PostContext
 	)
-
+	
 	const choosePost = postId => {
 		findPost(postId)
 		setShowUpdatePostModal(true)
 	}
-
+	const onClickPlay = () => {
+		setShow(true)
+	}
+	const idvideo = url.indexOf("=")
+	const urlID = url.substring(idvideo + 1, idvideo + 12)
+	console.log("abc", urlID)
 	return (
 		<>
-			<Button className='post-button' href={url} target='_blank'>
+			<Button className='post-button' target='_blank' onClick={() => onClickPlay()}>
 				<img src={playIcon} alt='play' width='32' height='32' />
 			</Button>
-			
+
 			<Button className='post-button' onClick={choosePost.bind(this, _id)}>
 				<img src={editIcon} alt='edit' width='24' height='24' />
 			</Button>
 			<Button className='post-button' onClick={deletePost.bind(this, _id)}>
 				<img src={deleteIcon} alt='delete' width='24' height='24' />
 			</Button>
+			<Modal className='modal_block' size='xl' show={show} onHide={() => setShow(false)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Thông tin bài học</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div className='block-youtube'>
+						<iframe allowfullscreen={true} width='60%' height='100%' src={`https://youtube.com/embed/${urlID}`} title="abc" ></iframe>
+						<TextEditor />
+					</div>
+				</Modal.Body>
+			</Modal>
 		</>
 	)
 }
--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,17 +1,19 @@
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import TextEditor from '../components/layout/TextEditor'
 
 const About = () => {
 	return (
 		<Row className='mt-5' style={{ marginRight: 0 }}>
 			<Col className='text-center'>
+				
 				<Button
 					variant='primary'
-					href='https://www.youtube.com/c/HenryWebDev'
+					href='https://www.facebook.com/dvcuong26'
 					size='lg'
 				>
-					Visit my channel for more tutorials
+					Đây là About
 				</Button>
 			</Col>
 		</Row>
