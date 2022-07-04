6303818332653982e8d2583595838ecf584aa5a1
Admin <dovancuong160700@gmail.com>
2022-07-04 20:29:22 +0700

    done

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -7,7 +7,6 @@ const About = () => {
 	return (
 		<Row className='mt-5' style={{ marginRight: 0 }}>
 			<Col className='text-center'>
-				
 				<Button
 					variant='primary'
 					href='https://www.facebook.com/dvcuong26'
